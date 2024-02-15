5c73d6c7f915cd70f36f92d51f8448dc8e28c3cd
Nikesh Jagdish Malik <84899873+nikeshmalik66@users.noreply.github.com>
2024-02-15 16:12:37 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+from wsgi import app
